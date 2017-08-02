6ea7b305bee02f9c5d6f9512457d68ec3936cdeb
Ryan Angelo <cadagryanangelo@gmail.com>
2017-08-02 22:19:45 +0800

    spec for testing classes

--- spec/hello_crush_spec.rb
+++ spec/hello_crush_spec.rb
@@ -1,11 +1,15 @@
-require "spec_helper"
+require 'spec_helper'
 
 RSpec.describe HelloCrush do
-  it "has a version number" do
+  it 'has a version number' do
     expect(HelloCrush::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it 'will return random message' do
+    expect(HelloCrush::Say.new('Mae').greet).not_to be_empty
+  end
+
+  it 'will return thing to give' do
+    expect(HelloCrush::Give.new('Mae').for_you).not_to be_empty
   end
 end
