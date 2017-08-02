3601fe2ee5976ce8b202ade9f0def84d10c577d6
Ryan Angelo <cadagryanangelo@gmail.com>
2017-08-02 22:17:33 +0800

    messages constant

---
+++ lib/hello_crush/messages.rb
@@ -0,0 +1,9 @@
+module HelloCrush
+  MESSAGES = ['You have been living in my dreams for a long time, what about
+               making it real for once?', 'Your kisses and your love are the
+               only medicine for my broken heart.', 'When I am asleep I dream
+               of you, but when I am awake, I desire you all day long.', 'Your
+               lips are my obsession. I cannot imagine a life without
+               you.', 'I don’t know what to do with you… but neither I know
+               what to do without you.'].freeze
+end
