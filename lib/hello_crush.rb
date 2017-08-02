b66c9ad16daf6bfe3bdc4a3295a963b17c60fd05
Ryan Angelo <cadagryanangelo@gmail.com>
2017-08-02 22:19:30 +0800

    require classes and constants

--- lib/hello_crush.rb
+++ lib/hello_crush.rb
@@ -1,4 +1,8 @@
-require "hello_crush/version"
+require 'hello_crush/version'
+require 'hello_crush/say'
+require 'hello_crush/give'
+require 'hello_crush/messages'
+require 'hello_crush/things'
 
 module HelloCrush
   # Your code goes here...
